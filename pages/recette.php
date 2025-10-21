d4eab89555bda74c141113ab5ac099a2d4a9d3cb
Harena RANDRIAMANJATO <r.harena26@gmail.com>
2025-10-21 14:38:56 +0300

    recette

---
+++ pages/recette.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../assets/bootstrap5_3/bootstrap/css/bootstrap.min.css">
+    <script src="../assets/bootstrap5_3/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <title>recette</title>
+</head>
+<body>
+    
+</body>
+</html>
